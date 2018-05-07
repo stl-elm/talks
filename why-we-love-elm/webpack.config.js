25d276f15ace6afc0b494c8fef72cbc6d2c2618e
Abadi Kurniawan <abadi.kurniawan@gmail.com>
2018-05-07 13:48:49 -0500

    Change dist folder location

--- why-we-love-elm/webpack.config.js
+++ why-we-love-elm/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/dist",
+    publicPath: "./dist",
   },
   plugins: [
     new webpack.NamedModulesPlugin(),
